dcee0623777f7aceaf1ef1aeb839228744632850
bensooraj <bensooraj@users.noreply.github.com>
2024-06-29 22:40:26 +0530

    tags

--- base_stack/ssm_parameter.tf
+++ base_stack/ssm_parameter.tf
@@ -3,4 +3,5 @@ resource "aws_ssm_parameter" "kms_key_id" {
   name  = "/${local.environment}/${local.stack_name}/kms_key_id"
   description = "KMS key ID for base stack"
   value = aws_kms_key.base_stack.key_id
+  tags = var.default_tags
 }
